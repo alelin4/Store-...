2cdecabd345b357d7b7d50ad7973978349ea93e2
Naviddev1 <Navid86@hotmail.se>
2023-05-26 15:01:49 +0200

    som bugs and fixes

--- webshop_api_react/client/vite.config.ts
+++ webshop_api_react/client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+ export default defineConfig({
   server: {
     proxy: {
       "/api": {
@@ -12,3 +12,4 @@ export default defineConfig({
   },
   plugins: [react()],
 });
+
