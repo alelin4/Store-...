5d140cc2d836fb2944b767815fc35351710f2200
Walid Ahmeddin <112585598+walidata1@users.noreply.github.com>
2023-06-13 13:31:34 +0300

    Loader before comfirmation page

---
+++ webshop_api_react/client/src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+// Loader.tsx
+import React from 'react';
+
+const Loader: React.FC = () => {
+  return (
+    <div className="loader">
+        <h2>Processing your order...</h2>
+      <h2>Loading...</h2>
+      <p>Patience, it's almost there!</p>
+    </div>
+  );
+};
+
+export default Loader;
--- webshop_api_react/client/src/components/OrderConfirmation/OrderConfirmation.tsx
+++ webshop_api_react/client/src/components/OrderConfirmation/OrderConfirmation.tsx
@@ -1,10 +1,34 @@
-function OrderConfirmation() {
+// ConfirmationPage.tsx
+import React, { useState, useEffect } from "react";
+import Loader from "../Loader/Loader";
+
+const ConfirmationPage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulate an asynchronous operation, e.g., processing order, etc.
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false); // Set isLoading to false after the desired delay
+    }, 2000); // Simulated delay of 2 seconds
+  }, []);
+
   return (
-    <div>
-      <h2>Order Confirmation</h2>
-      <p>Your order has been confirmed. Thank you for shopping with us!</p>
+    <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
+      <div className=" p-2 border border-gray-200 rounded-lg">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="confirmation-content">
+             <h1 className="text-2xl font-bold mb-4">Orderbekräftelse</h1>
+            <p>
+              Din beställning har bekräftats. Tack för att du handlat med oss!
+              Order id:
+            </p>
+          </div>
+        )}
+      </div>{" "}
     </div>
   );
-}
+};
 
-export default OrderConfirmation;
+export default ConfirmationPage;
