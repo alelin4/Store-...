1349c5891b7f1d2e6e232409106e7c6d966f7423
Walid Ahmeddin <112585598+walidata1@users.noreply.github.com>
2023-06-13 11:07:42 +0300

    Editing CartDetails

--- webshop_api_react/client/src/components/CartDetails/CartDetails.tsx
+++ webshop_api_react/client/src/components/CartDetails/CartDetails.tsx
@@ -1,13 +1,49 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../Context-reducer/StoreContext";
-import ProductCard from "../ProductCard/ProductCard";
 import { Link } from "react-router-dom";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import { GrCart } from "react-icons/gr";
 
 function CartDetails() {
-  const { products, total } = useContext(StoreContext);
-  const isLoggedIn = Cookies.get('token') !== undefined;
+  const { products, total, addToCart, removeFromCart } =
+    useContext(StoreContext);
+  const isLoggedIn = Cookies.get("token") !== undefined;
+
+  const getProductCount = (productId) => {
+    const count = products.filter(
+      (product) => product._id === productId
+    ).length;
+    return count;
+  };
+
+  const getProductPrice = (productId) => {
+    const product = products.find((product) => product._id === productId);
+    if (product) {
+      return product.price * getProductCount(productId);
+    }
+    return 0;
+  };
+
+  const handleAddQuantity = (productId) => {
+    const product = products.find((product) => product._id === productId);
+    if (product) {
+      addToCart(product);
+    }
+  };
+
+  const handleRemoveQuantity = (productId) => {
+    const product = products.find((product) => product._id === productId);
+    if (product) {
+      removeFromCart(product);
+    }
+  };
+
+  const handleRemoveItem = (productId) => {
+    const filteredProducts = products.filter(
+      (product) => product._id === productId
+    );
+    filteredProducts.forEach((product) => removeFromCart(product));
+  };
 
   return (
     <div className="p-10 mx-auto px-8 border border-gray-200 rounded-lg">
@@ -16,21 +52,50 @@ function CartDetails() {
         <div>
           <p className="text-xl p-6">Din varukorg är tom.</p>
           <div className="p-6 text-9xl bg-white">
-          <GrCart />
-        </div>
+            <GrCart />
+          </div>
         </div>
       ) : (
-        <ul className="mb-4">
-          <div>
-            <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
-              <h2>Dina Produkter</h2>
-              <p> Pris för produkter: {total}:-</p>
-            </div>
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+        <div>
+          <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
+            <h2>Dina Produkter</h2>
+            <p> Pris för produkter: {total}:-</p>
           </div>
-        </ul>
+          <ul className="mb-4">
+            {Array.from(new Set(products.map((product) => product._id))).map(
+              (productId) => (
+                <li key={productId} className="p-2">
+                  <p>
+                    Product ID: {productId}, Quantity:{" "}
+                    <button
+                      className="px-2 border border-gray-400 rounded"
+                      onClick={() => handleRemoveQuantity(productId)}
+                    >
+                      -
+                    </button>{" "}
+                    {getProductCount(productId)}{" "}
+                    <button
+                      className="px-2 border border-gray-400 rounded"
+                      onClick={() => handleAddQuantity(productId)}
+                    >
+                      +
+                    </button>
+                    <button
+                      className="px-2 ml-2 text-red-500"
+                      onClick={() => handleRemoveItem(productId)}
+                    >
+                      Remove
+                    </button>
+                  </p>
+                  <p>
+                    Price: {getProductPrice(productId)}{" "}
+                    {/* Assuming the price is in SEK */}
+                  </p>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       )}
       {products.length > 0 && (
         <Link to={isLoggedIn ? "/checkout" : "/login"}>
--- webshop_api_react/client/src/components/Checkout/Checkout.tsx
+++ webshop_api_react/client/src/components/Checkout/Checkout.tsx
@@ -1,10 +1,25 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, ChangeEvent, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { StoreContext } from "../../Context-reducer/StoreContext";
-import ProductCard from "../ProductCard/ProductCard";
+import { StoreContext, Product } from "../../Context-reducer/StoreContext";
 
+interface UserDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  street: string;
+  zipcode: string;
+  city: string;
+  country: string;
+  shippingOption: string;
+}
+interface ShippingMethod {
+  _id: string;
+  company: string;
+  price: number;
+  deliveryTimeInHours: number;
+}
 function Checkout() {
-  const [userDetails, setUserDetails] = useState({
+  const [userDetails, setUserDetails] = useState<UserDetails>({
     firstName: "",
     lastName: "",
     email: "",
@@ -14,11 +29,11 @@ function Checkout() {
     country: "",
     shippingOption: "",
   });
-  const [errors, setErrors] = useState({}); // Track form validation errors
 
+  const [errors, setErrors] = useState<Partial<UserDetails>>({}); // Track form validation errors
   const navigate = useNavigate();
   const { products, total } = useContext(StoreContext);
-  const [shippingMethods, setShippingMethods] = useState([]);
+  const [shippingMethods, setShippingMethods] = useState<ShippingMethod[]>([]);
   const [shippingPrice, setShippingPrice] = useState(0);
   const totalWithShipping = total + shippingPrice;
 
@@ -31,71 +46,59 @@ function Checkout() {
       })
       .catch((error) => {
         console.error(error);
-        // Handle error state if needed
       });
   }, [total]);
 
-  const handleShippingOptionChange = (e) => {
+  const handleShippingOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedShippingOption = shippingMethods.find(
       (option) => option._id === e.target.value
     );
-
     const price = selectedShippingOption ? selectedShippingOption.price : 0;
-
     setShippingPrice(price);
-
     setUserDetails({
       ...userDetails,
       shippingOption: e.target.value,
     });
   };
-
   const validateForm = () => {
     let isValid = true;
-    const errors = { street, zipcode, city, country, shippingOption };
-
+    const errors: Partial<UserDetails> = {};
     if (!userDetails.street) {
       errors.street = "Vänligen ange en gatuadress.";
       isValid = false;
     }
-
     if (!userDetails.zipcode) {
       errors.zipcode = "Vänligen ange ett postnummer.";
       isValid = false;
     }
-
     if (!userDetails.city) {
       errors.city = "Vänligen ange en stad.";
       isValid = false;
     }
-
     if (!userDetails.country) {
       errors.country = "Vänligen ange ett land.";
+
       isValid = false;
     }
-
     if (!userDetails.shippingOption) {
       errors.shippingOption = "Vänligen välj en leveransmetod.";
       isValid = false;
     }
-
     setErrors(errors);
     return isValid;
   };
-
-  const placeOrder = () => {
+  const placeOrder = async () => {
     const isValid = validateForm();
-
     if (!isValid) {
       return;
     }
-
+    const orderItems = products.map((product: Product) => ({
+      product: product._id,
+      quantity: getProductCount(product._id), // Get the quantity of the product
+      price: product.price * getProductCount(product._id),
+    }));
     const orderData = {
-      orderItems: products.map((product) => ({
-        product: product.id,
-        quantity: product.quantity,
-        price: product.price * product.quantity,
-      })),
+      orderItems: orderItems,
       deliveryAddress: {
         street: userDetails.street,
         zipcode: userDetails.zipcode,
@@ -104,56 +107,91 @@ function Checkout() {
       },
       shippingMethod: userDetails.shippingOption,
     };
-
-    fetch("/api/orders", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(orderData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Assuming the response contains the newly created order object
+    try {
+      const response = await fetch("/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+      });
+      console.log(orderData);
+      if (response.ok) {
+        const data = await response.json();
         console.log(data); // Log the response data
         navigate("/order-confirmation"); // Navigate to the Order Confirmation page
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle error state if needed
-      });
+      } else {
+        console.error("Error:", response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
   useEffect(() => {
     // Fetch user details from MongoDB when the component mounts
-    fetch("/api/users/authorize") // Replace with the actual API endpoint to retrieve user data
+    fetch("/api/users/authorize")
       .then((response) => response.json())
+
       .then((data) => {
-        // Assuming the response contains the user details (firstName, lastName, email)
-        setUserDetails({
-          ...userDetails,
+        setUserDetails((prevUserDetails) => ({
+          ...prevUserDetails,
+
           firstName: data.firstName,
+
           lastName: data.lastName,
+
           email: data.email,
-        });
+        }));
       })
+
       .catch((error) => {
         console.error(error);
-        // Handle error state if needed
       });
   }, []);
+  const totalProductPrice = (productId) => {
+    const product = products.find((product) => product._id === productId);
+    if (product) {
+      return product.price * getProductCount(productId);
+    }
+    return 0;
+  };
+  const getProductPrice = (productId) => {
+    const product = products.find((product) => product._id === productId);
+    if (product) {
+      return product.price * 1;
+    }
+    return 0;
+  };
+  const getProductCount = (productId) => {
+    const count = products.filter(
+      (product) => product._id === productId
+    ).length;
+    return count;
+  };
 
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Kassa</h1>
       <ul className="mb-4">
         <div>
-          <div className="flex flex-row items-center justify-between mt-2 py-6 px-3 text-xl font-medium">
-            <h2 className="text-2xl font-bold ">Dina produkter</h2>
-            <p>Pris för produkter: {total}:-</p>
+          <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
+            <h2>Dina Produkter</h2>
+            <p> Pris för produkter: {total}:-</p>
           </div>
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
+          <ul className="mb-4">
+            {Array.from(new Set(products.map((product) => product._id))).map(
+              (productId) => (
+                <li key={productId} className="p-2">
+                  <p>
+                    Product ID: {productId}, Quantity:{" "}
+                    {getProductCount(productId)}, product price:{" "}
+                    {getProductPrice(productId)} , Total:{" "}
+                    {totalProductPrice(productId)}
+                  </p>
+                </li>
+              )
+            )}
+          </ul>
         </div>
       </ul>
       <div className="grid grid-cols-2 gap-3 mb-4">
