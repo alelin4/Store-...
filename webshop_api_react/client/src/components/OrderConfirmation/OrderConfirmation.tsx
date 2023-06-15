2d07855329f740e5f4e7b4ea5f63528b007d8c38
Walid Ahmeddin <112585598+walidata1@users.noreply.github.com>
2023-06-15 12:09:29 +0300

    info in ord. comf. page

--- webshop_api_react/client/src/components/OrderConfirmation/OrderConfirmation.tsx
+++ webshop_api_react/client/src/components/OrderConfirmation/OrderConfirmation.tsx
@@ -13,6 +13,8 @@ const ConfirmationPage: React.FC = () => {
       setIsLoading(false); // Set isLoading to false after the desired delay
     }, 2000); // Simulated delay of 2 seconds
   }, []);
+ 
+
 
   return (
     <div className="flex flex-row items-center justify-between mt-2 py-6 px-10 text-xl font-medium">
@@ -23,34 +25,24 @@ const ConfirmationPage: React.FC = () => {
           <div className="confirmation-content">
             <h1 className="text-2xl font-bold mb-4">Orderbekräftelse</h1>
             <p>
-              Din beställning har bekräftats. Tack för att du handlat med oss!
-              Order id: {orderData.orderNumber}
-            </p>
-
-            <h2>Order Items</h2>
-            <ul>
-              {orderData.orderItems.map((item: any, index: number) => (
-                <li key={index}>
-                  <p>Product: {item.product}</p>
-                  <p>Quantity: {item.quantity}</p>
-                  <p>Price: {item.price}</p>
-                </li>
-              ))}
-            </ul>
-            <h2>User Details</h2>
-            <ul>
-              <li>
-                <p>First Name: {userDetails.firstName}</p>
-                <p>Last Name: {userDetails.lastName}</p>
-                <p>Email: {userDetails.email}</p>
-              </li>
-            </ul>
-
-            <p>Shipping Method: {orderData.shippingMethod}</p>
-            <p>
-              Delivery Address: {orderData.deliveryAddress.street},{" "}
+              Din beställning har bekräftats. Tack för att du handlat med oss{" "}
+              {userDetails.firstName} ! Ditt order id är:{" "}
+              {orderData.orderNumber} och du kommer få din order levererad till{" "}
+              {orderData.deliveryAddress.street},{" "}
               {orderData.deliveryAddress.zipcode}{" "}
-              {orderData.deliveryAddress.city}, {orderData.deliveryAddress.country}
+              {orderData.deliveryAddress.city},{" "}
+              {orderData.deliveryAddress.country}.            </p>
+      
+
+            
+{orderData.shippingMethod === "648248d3bbcfe7d8092c84f8" && <p>Du kommer få din beställning via DHL inom 24Tim</p>}
+{orderData.shippingMethod === "648248eabbcfe7d8092c84fa" && <p>Du kommer få din beställning via Instabox inom 48Tim</p>}
+{orderData.shippingMethod === "648253e3bbcfe7d8092c8544" && <p>Du kommer få din beställning via Postnord inom 72Tim</p>}
+
+
+            <p>
+              Du kommer få ett kvitto på ditt köp på detta mejladress:{" "}
+              {userDetails.email}
             </p>
           </div>
         )}
